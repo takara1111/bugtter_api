d9b12480f9d4984b40a2fced53742fac311992f9
takara1356 <goblin7283@gmail.com>
2020-03-19 23:06:16 +0900

    [change] authenticateメソッドを無効化

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::API
   include ActionController::HttpAuthentication::Token::ControllerMethods
 
-  before_action :authenticate!
+  # before_action :authenticate!
 
   private
 
