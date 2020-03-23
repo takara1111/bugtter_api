eb8255d34bc3ae2cbd0ab5f4a9469ae07c2beba1
takara1356 <goblin7283@gmail.com>
2020-03-23 21:46:50 +0900

    [add] ログイン処理を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_17_114411) do
+ActiveRecord::Schema.define(version: 2020_03_22_130126) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2020_03_17_114411) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
     t.string "token"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
