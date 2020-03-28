3bca2e30c2baa3afcb8c08823724294183657d9d
takara1356 <goblin7283@gmail.com>
2020-03-28 23:31:07 +0900

    [add] indexとshowメソッドをuserに追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,12 +7,13 @@ class UsersController < ApplicationController
   def index
     users = User.all
 
-    render json: users
+    render json: users, only:[:id, :name]
   end
 
   # GET /users/1
   def show
-    render json: user
+    user = User.find(params[:id])
+    render json: user, only:[:id, :name]
   end
 
   # POST /users
